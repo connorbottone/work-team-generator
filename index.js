82726817a7c1a7e17a8f349db6bcc6a98b0fb05f
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-01-06 23:11:14 -0500

    created prompt to add additonal employees that will either add intern engineer or complete the propmpt and write our file, added annotations to the function to describe what is occuring

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Manager = require ("./lib/Manager");
-const Intern = require ("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 const inquirer = require("inquirer");
 const path = require("path");
@@ -21,55 +21,55 @@ const Employee = require("./lib/Employee");
 // 4.
 // create an empty employee memeber array variable to store the employee members, manager, engineers, and interns
 // create an empty employee id array to store the employee ids
-const employeeMembers=[];
+const employeeMembers = [];
 const employeeIds = [];
 // 5.
 // print user of usage
 
 //inital fuction to begin the program that will initiate the manager prompt
-function beginprompt(){
+function beginprompt() {
     managerPrompt();
 }
 //This prompt will take in the inputs of a name,id,email and office number then return all the vlaues as a new manager
-function managerPrompt(){
-    inquirer .prompt([
+function managerPrompt() {
+    inquirer.prompt([
         {
-            type:"input",
-            name:"name",
-            message:"Please Enter Your Team Managers name",
+            type: "input",
+            name: "name",
+            message: "Please Enter Your Team Managers name",
         },
         {
-            type:"input",
-            name:"id",
-            message:"Enter your Managers ID number:",
+            type: "input",
+            name: "id",
+            message: "Enter your Managers ID number:",
         },
         {
-            type:"input",
-            name:"email",
-            message:"Please Enter Managers Email Adress",
+            type: "input",
+            name: "email",
+            message: "Please Enter Managers Email Adress",
         },
         {
-            type:"input",
-            name:"officeNumber",
-            message:"Enter managers Office number",
+            type: "input",
+            name: "officeNumber",
+            message: "Enter managers Office number",
         }
-        
-    ])
-    .then((userRes)=>{
-        const manager = new Manager(
-            userRes.name,
-            userRes.id,
-            userRes.email,
-            userRes.officeNumber
-        );
-        //adding the new manager to our employee array and adding the managers id into our id array
-        employeeMembers.push(manager)
-        employeeIds.push(manager.id)
-        //calling our next function to run to add more team members if nescassary
-        addEmployee();
 
-    }
-    );
+    ])
+        .then((userRes) => {
+            const manager = new Manager(
+                userRes.name,
+                userRes.id,
+                userRes.email,
+                userRes.officeNumber
+            );
+            //adding the new manager to our employee array and adding the managers id into our id array
+            employeeMembers.push(manager)
+            employeeIds.push(manager.id)
+            //calling our next function to run to add more team members if nescassary
+            addEmployee();
+
+        }
+        );
 }
 
 // 8.
@@ -79,7 +79,31 @@ function managerPrompt(){
 // - make call to add-engineer-function if the choice is engineer
 // - make call to add-intern-function if choice is intern
 // - make call to build-team function if choice is end of adding employee
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "employeePosition",
+            message: "Would you like to add anyother employees?",
+            choices: ["Add Intern", "Add Engineer", "No I am Finished"]
 
+        },
+    ])
+        //if the users input to the addemplyes prompt is add intern the internprompt function will bhe excecuded
+        .then((userRes) => {
+            if (userRes.employeePosition === "Add Intern") {
+                internPrompt();
+            }
+            //if the users input to the addemplyes prompt is add enginerr the engineerprompt function will be excecuded
+            else if (userRes.employeePosition === "Add Engineer") {
+                engineerPrompt();
+            }
+            //if the users input to the addemplyes prompt is No i am finished the function to create file will be exccuted,There is no need to make this statement = to the userres of no i am finished for this is the last possible ooptions that can be ran if the other to options are null
+            else{
+                generateFile();
+            }
+        });
+}
 // 8.
 // add engineer function
 // - prompt user with questions for engineer name, id, email, and github name
